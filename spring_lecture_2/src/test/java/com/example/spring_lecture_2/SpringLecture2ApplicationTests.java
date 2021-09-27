c2d90a161ec3a236dfb259b48ec71ed9dbf59b14
gyounghee <51191248+gyounghee@users.noreply.github.com>
2021-09-27 12:07:35 +0900

    Add files via upload

---
+++ spring_lecture_2/src/test/java/com/example/spring_lecture_2/SpringLecture2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_lecture_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLecture2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
